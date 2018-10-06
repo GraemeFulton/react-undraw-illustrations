e7bb6b4de8b03c4838db30f6a051d70267d5257f
Lena Venable <venablena@gmail.com>
2018-10-06 13:21:06 -0700

    change comments

--- svgr_util/svgr.config.js
+++ svgr_util/svgr.config.js
@@ -22,8 +22,6 @@ const unDrawConfig = {
 
 module.exports =  unDrawConfig;
 
-//A fix for better formatting of the style attribute was committed right when I
-//was writing this but because I'm not 100% sure how well it works,
-//I prefer not trying it.
+//A fix for better formatting of svgProps that might enable customizing the //style attribute directly was committed right when I was writing this but //because I'm not 100% sure how well it works, I prefer not trying it.
 //It is possible that in the future a replaceAttrValues option will be updated
 //to enable directly assigning {props.primaryColor} so that you don't need to //do it manually by search and replace.
